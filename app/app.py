723e859f7611d88c613f7124bc5b3fbf77005097
Mirza <mirzawell.05@gmail.com>
2026-05-06 14:58:42 +0600

    first commit

---
+++ app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevOps Flask Project is running!"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+@app.route("/users")
+def users():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM users;")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return {"users": [row[0] for row in rows]}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
